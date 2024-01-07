67b6b242a74e0f27e8e5d6a472e3e9451f1ff9df
Jhonatan Gamboa <jhntngmb@gmail.com>
2024-01-06 19:36:32 -0500

    configura env y readme

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'root',
-      password: 'password',
-      database: 'redsocial_db',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 3307,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'redsocial_db',
       autoLoadEntities: true,
       synchronize: true,
     }),
